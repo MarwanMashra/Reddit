3f85c34b873004bf60f9f732b3d8d74022ea40ee
torpid <troyfau@gmail.com>
2020-02-08 17:45:00 +0100

    Improved location search with comment exploration

--- Scraper_Python/reddit.py
+++ Scraper_Python/reddit.py
@@ -3,6 +3,31 @@
 
 import json, os, pprint, praw, re, requests, treetaggerwrapper
 
+
+
+def GeoNamesQuery(location,dic_results) :
+	url="http://api.geonames.org/searchJSON"
+	data="?q="+location+"&country=FR&username=scrapelord" #username inséré dans data
+	search_res=requests.get(url+data,auth=("scrapelord","Blorp86"))
+	#print(search_res.encoding) #UTF-8
+	print("Status code de la requête GeoNames:",search_res.status_code,"\n") #200:OK, 401:non autorisé
+	search_res=search_res.json() #Décodeur JSON appliqué à l'objet Response renvoyé par la requête
+	if search_res['totalResultsCount'] != 0:
+		dic_results['TotalResults']+=1
+		dic_tmp={}
+		dic_tmp['lng']=search_res['geonames'][0]['lng']	#Longitude
+		dic_tmp['lat']=search_res['geonames'][0]['lat']	#Latitude
+		dic_tmp['img']=post.url	#Lien direct vers la photo
+		dic_results['Results'].append(dic_tmp)
+		print("Premier résultat Geonames: ",search_res["geonames"][0]["toponymName"])
+		pprint.pprint(dic_tmp)
+		print("\n###############")
+		return True
+	else:
+		return False
+
+
+
 reddit=praw.Reddit(client_id="v7xiCUUDI3vEmg",client_secret="5Q6FHHJT-SW0YRnEmtWkekWsxHU",
 password="Blorp86",user_agent="PhotoScraper",username="scrapelord")
 
@@ -18,11 +43,10 @@ reddit_tagger=treetaggerwrapper.TreeTagger(TAGLANG="en",TAGDIR=os.getcwd()+"/Tre
 
 #Résultats de la recherche dans le subreddit
 test_posts=target_sub.search(query,limit=15)
-dic_results={}	#Dico final stockant tous les résultats de recherche
-dic_tmp={} #Dico temporaire pour un résultat de recherche
+dic_results={}	#Dico stockant tous les résultats de recherche
 dic_results['TotalResults']=0
 dic_results['Results']=[]
-for post in test_posts:
+for post in test_posts: #Objets 'submission'
 	if re.search(".*"+country+"[.,/[( ]",post.title): #Pays suivi de '.' ',' '/' '[' '(' ou ' '
 		res=re.search("^([^[(]+)",post.title) #Match tous les caractères depuis le début de la ligne, excluant [ et (
 		if res:
@@ -44,26 +68,43 @@ for post in test_posts:
 					location=reddit_tags[i][0]+" "+location
 					if i != 0 and reddit_tags[i-1][1] != "NP0":
 						break
+			#Si le lieu n'a pas été trouvé, on parcourt la liste à la suite du mot 'NomDuPays'
+			if location == "":
+				if len(reddit_tags) > idx_upper_range+1: #La liste contient au moins un élément après 'NomDuPays'
+					for i in range(idx_upper_range+1,len(reddit_tags)):
+						if reddit_tags[i][1] == "NP0":
+							location+=reddit_tags[i][0]+" "
+							if i != len(reddit_tags)-1 and reddit_tags[i+1][1] != "NP0":
+								break
+			#Si le lieu n'a toujours pas été trouvé, on explore les commentaires
+			if location == "":
+				all_comments=post.comments.list() #Liste du parcours en largeur de l'arborescence de commentaires
+				for comment in all_comments:
+					if isinstance(comment,praw.models.MoreComments): #On ignore les objets MoreComments (pour le moment?)
+						continue
+					comment_tags=treetaggerwrapper.make_tags(reddit_tagger.tag_text(comment.body))
+					pprint.pprint(comment_tags)
+					for i in range(0,len(comment_tags)):
+						if comment_tags[i][0].isalpha() and comment_tags[i][1] == "NP0" and (i == len(comment_tags)-1 or comment_tags[i+1][0] != "?"):
+							location+=comment_tags[i][0]+" "
+							if i != len(comment_tags)-1 and comment_tags[i+1][1] != "NP0":
+								break
+						#Si le nom propre est suivi d'un '?', on cherche la réponse
+						if i != len(comment_tags)-1 and comment_tags[i+1][0] == "?":
+							for reply in all_comments:
+								if reply.parent_id == comment.id:
+									reply_tags=treetaggerwrapper.make_tags(reddit_tagger.tag_text(reply.body))
+									for j in range(0,len(reply_tags)):
+										if reply_tags[j][0] in ["yes", "Yes", "yeah", "Yeah"]:
+											location+=comment_tags[i][0]
+											break
+						if location != "":
+							break
 			print("Lieu trouvé: ",location)
 
 			#GeoNames
 			if location != "":
-				url="http://api.geonames.org/searchJSON"
-				data="?q="+location+"&country=FR&username=scrapelord" #username inséré dans data
-				search_res=requests.get(url+data,auth=("scrapelord","Blorp86"))
-				#print(search_res.encoding) #UTF-8
-				print("Status code de la requête GeoNames:",search_res.status_code,"\n") #200:OK, 401:non autorisé
-				search_res=search_res.json() #Décodeur JSON appliqué à l'objet Response renvoyé par la requête
-				if search_res['totalResultsCount'] != 0:
-					dic_results['TotalResults']+=1
-					dic_tmp={}
-					dic_tmp['lng']=search_res['geonames'][0]['lng']	#Longitude
-					dic_tmp['lat']=search_res['geonames'][0]['lat']	#Latitude
-					dic_tmp['img']=post.url	#Lien direct vers la photo
-					dic_results['Results'].append(dic_tmp)
-					print("Premier résultat Geonames: ",search_res["geonames"][0]["toponymName"])
-					pprint.pprint(dic_tmp)
-					print("\n###############")
+				GeoNamesQuery(location,dic_results)
 			else:
 				print("")
 
