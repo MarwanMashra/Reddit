039e1d7d6b6fd1e92a67be2d973f7a511317745f
torpid <troyfau@gmail.com>
2020-02-21 08:22:51 +0100

    Arrangé dico_temporaire et appels geonames, rajouté stockage résultats sur MongoDB avec classe dédiée

--- Scraper_Python/reddit.py
+++ Scraper_Python/reddit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-import collections, json, more_itertools, os, pprint, praw, re, requests, sys, treetaggerwrapper
+import collections, copy, json, mongo, more_itertools, os, pprint, praw, re, requests, sys, treetaggerwrapper
 
 
 
@@ -33,7 +33,7 @@ def geonames_query(location, dic_results, dic_tmp, fuzzy=False):
 			#Recherche des matchs exacts
 			prio_que = collections.deque() #Meilleure complexité à l'insertion que list
 			for res in search_res['geonames']:
-				if res["name"] == location:
+				if res["name"].lower() == location.lower():
 					if res["fcl"] in ["A","P","R","S"]:
 						prio_que.append(res)
 					else:
@@ -42,18 +42,17 @@ def geonames_query(location, dic_results, dic_tmp, fuzzy=False):
 				dic_tmp["name"] = prio_que[0]["name"]
 				dic_tmp["lng"] = prio_que[0]["lng"]
 				dic_tmp["lat"] = prio_que[0]["lat"]
-				dic_tmp["fcl"] = prio_que[0]["fcl"]
+				dic_tmp["featureclass"] = prio_que[0]["fcl"]
 			#Si pas de matchs exacts
 			else:
 				dic_tmp["name"] = search_res["geonames"][0]["name"]
 				dic_tmp['lng'] = search_res['geonames'][0]['lng']
 				dic_tmp['lat'] = search_res['geonames'][0]['lat']
-				dic_tmp["fcl"] = search_res["geonames"][0]["fcl"]
-			dic_tmp["loc"] = location
+				dic_tmp["featureclass"] = search_res["geonames"][0]["fcl"]
+			dic_tmp["location"] = location
 			dic_results['Results'].append(dic_tmp)
 			print("Premier résultat Geonames: ",search_res["geonames"][0]["name"])
-			print("Meilleur résultat Geonames: ",dic_tmp["name"]+"\t"+dic_tmp["fcl"])
-			print("\n###############")
+			print("Meilleur résultat Geonames: ",dic_tmp["name"]+"\t"+dic_tmp["featureclass"])
 			return True
 		else:
 			return False
@@ -79,8 +78,9 @@ reddit=praw.Reddit(client_id="v7xiCUUDI3vEmg",client_secret="5Q6FHHJT-SW0YRnEmtW
 password="Blorp86",user_agent="PhotoScraper",username="scrapelord")
 
 #Configuration recherche reddit
-target_sub = reddit.subreddit("EarthPorn") #Subreddit cible
-country = "France" #Pays cible
+rgnversion = sys.argv[1] #Initialement 1.00
+target_sub = reddit.subreddit("EarthPorn")
+country = "France"
 query = "title:"+country
 print("\033[92m"+target_sub.display_name+"\033[0m",
 	"\nRésultats de recherche pour les soumissions reddit avec: ",query,"\n")
@@ -90,8 +90,8 @@ reddit_tagger = treetaggerwrapper.TreeTagger(TAGLANG="en",TAGDIR=os.getcwd()+"/T
 #Se placer dans un dossier pour que le chemin TAGDIR fonctionne
 
 #Résultats de la recherche dans le subreddit
-test_posts = target_sub.search(query,limit=20)
-dic_results = {}	#Dico stockant tous les résultats de recherche
+test_posts = target_sub.search(query,limit=25)
+dic_results = {}
 dic_results['TotalResults'] = 0
 dic_results['Results'] = []
 for post in test_posts: #Objets 'submission'
@@ -110,7 +110,7 @@ for post in test_posts: #Objets 'submission'
 		location_list = []
 		#Recherche prioritaire du lieu: la liste jusqu'au mot 'NomDuPays'
 		locationfinder(location_list,more_itertools.peekable(reddit_tags[:title_split]))
-		location_list.reverse() #Les noms propres les plus proches de 'NomDuPays' sont prioritaires
+		#location_list.reverse()
 		#Priorité secondaire: la liste à la suite du mot 'NomDuPays'
 		if len(reddit_tags) > title_split+1:
 			locationfinder(location_list,more_itertools.peekable(reddit_tags[title_split+1:]))
@@ -128,21 +128,26 @@ for post in test_posts: #Objets 'submission'
 
 		#GeoNames
 		if location_list:
-			i = dic_results["TotalResults"]
+			dic_tmp = {}
+			dic_tmp["img_url"] = post.url
+			dic_tmp["search_version"] = rgnversion
+			dic_tmp["title"] = res.group(1).strip()
+			dic_tmp["taglist"] = reddit_tags
+			dic_tmp["location_list"] = location_list
 			for loc in location_list:
-				dic_tmp = {}
-				dic_tmp["img"] = post.url
-				dic_tmp["title"] = res.group(1).strip()
-				dic_tmp["taglist"] = reddit_tags
-				dic_tmp["loc_list"] = location_list
 				if geonames_query(loc,dic_results,dic_tmp):
 					break
 			"""Pas de résultat après avoir parcouru toute la liste: on passe à une fuzzy search pour
 			prendre en compte les potentielles erreurs d'orthographe"""
-			if dic_results["TotalResults"] == i:
+			if "location" not in dic_tmp:
 				for loc in location_list:
 					if geonames_query(loc,dic_results,dic_tmp,fuzzy=True):
 						break
+			if "location" in dic_tmp:
+				dic_tostore = copy.deepcopy(dic_tmp) #Pour ne pas passer dic_tmp lui-même à MongoSave
+				document = mongo.MongoSave(dic_tostore)
+				document.storeindb("Resultats_RGN")
+			print("\n###############")
 		else:
 			print("")
 
