272bf51b6da1173cd529c86f71e580b66de9fcec
torpid <troyfau@gmail.com>
2020-02-26 19:08:31 +0100

    Regex améliorée, rajout du nom geonames au dico, légère factorisation du code

--- Reddit_projet/script.py
+++ Reddit_projet/script.py
@@ -4,45 +4,69 @@
 import json, more_itertools, os, pprint, praw, re, requests,treetaggerwrapper
 from flask import Flask, render_template, request, jsonify
 
+
+'''Feature classes ('fcl')
+	A   Administrative divisions
+	H   Surface waters
+	L   Parks/reserves
+	P   Populated places
+	R   Roads
+	S   Structures
+	T   Mountains/Islands
+	U   Undersea
+	V   Woodlands
+'''
+
 def GeoNamesQuery(location, code_pays, dic_results, dic_tmp,img_url, fuzzy=False) :
-	url="http://api.geonames.org/searchJSON"
-	#Ici, FR est fixé: il y aura une variable code pays passé à la requête donc il faudra pouvoir trouver le code pays ISO 3166-1 alpha-2
-	data="?q="+location+"&country="+code_pays+"&username=scrapelord"
+	url='http://api.geonames.org/searchJSON'
+	data='?q='+location+'&country='+code_pays+'&username=scrapelord'
 	if fuzzy:
-		data+="&fuzzy=0.8" #Recherche fuzzy<1
-	search_res=requests.get(url+data,auth=("scrapelord","Blorp86"))
-	#print("Status code de la requête GeoNames:",search_res.status_code,"\n") #200:OK, 401:non autorisé
+		data+='&fuzzy=0.8' #Recherche fuzzy<1
+	search_res=requests.get(url+data,auth=('scrapelord','Blorp86'))
+	#print('Status code de la requête GeoNames:',search_res.status_code,'\n') #200:OK, 401:non autorisé
 	search_res=search_res.json() #Décodeur JSON appliqué à l'objet Response renvoyé par la requête
 	if search_res['totalResultsCount'] != 0:
 		dic_results['head']['total']+=1
-		print_res="" #Pour affichage test
+		print_res='' #Pour affichage test
+		prio_list=[]
 		for res in search_res['geonames']: #Recherche d'un match exact
-			if res['toponymName'] == location:
-				dic_tmp['lng']=res['lng']	#Longitude
-				dic_tmp['lat']=res['lat']	#Latitude
+			if res['name'].lower() == location.lower():
+				if res['fcl'] in ['A','P','R','S'] and not prio_list:
+					prio_list.append(res)
+				elif res['fcl'] in ['H','L','T','U','V']:
+					prio_list.insert(0,res)
+					break
+				dic_tmp['lng']=res['lng']
+				dic_tmp['lat']=res['lat']
 				print_res=res['toponymName']
 				break
-		if not dic_tmp: #Sinon premier résultat
-			dic_tmp['lng']=search_res['geonames'][0]['lng']	#Longitude
-			dic_tmp['lat']=search_res['geonames'][0]['lat']	#Latitude
+		if prio_list:
+			dic_tmp['name']=prio_list[0]['name']
+			dic_tmp['lng']=prio_list[0]['lng']
+			dic_tmp['lat']=prio_list[0]['lat']
+		else: #Sinon premier résultat
+			dic_tmp['name']=search_res['geonames'][0]['name']
+			dic_tmp['lng']=search_res['geonames'][0]['lng']
+			dic_tmp['lat']=search_res['geonames'][0]['lat']
 		dic_tmp['img']=img_url	#Lien direct vers la photo
 		dic_results['results'].append(dic_tmp)
-		print("Premier résultat Geonames: ",search_res["geonames"][0]["toponymName"])
-		print("Meilleur résultat Geonames: ",print_res)
-		print("\n###############")
+		print('Premier résultat Geonames: ',search_res['geonames'][0]['name'])
+		print('Meilleur résultat Geonames: ',print_res)
+		print('\n###############')
 		return True
 	else:
 		return False
 
 
 def IterTagList(location_list, p_iter) :
-	location=""
+	location=''
 	for word,pos,lemma in p_iter:
-		if pos == "NP" or  pos == "NP0": #Nom propre #'NP' pour Windows et 'NP0' pour Linux
-			location+=word+" "
-			if p_iter.peek(("end","end","end"))[1] != "NP" and p_iter.peek(("end","end","end"))[1] != "NP0": #Tuple par défaut passé à peek, retourné en fin d'itération
-				location_list.append(location.rstrip()) #Enlève le whitespace potentiel en fin de chaîne
-				location=""
+		if pos == 'NP' or  pos == 'NP0': #Nom propre 'NP' pour Windows et 'NP0' pour Linux
+			location+=word+' '
+			if p_iter.peek(('end','end','end'))[1] != 'NP' and \
+			p_iter.peek(('end','end','end'))[1] != 'NP0': #Tuple par défaut passé à peek, retourné en fin d'itération
+				location_list.append(location.rstrip())
+				location=''
 
 
 
@@ -55,39 +79,44 @@ def home():
 	return render_template('map.html')
 
 @app.route('/scraping',methods=['GET','POST'])
-def scraping():          #la fonction qui sera appeler par le JavaScript
+#La fonction appelée par la requête Javascript
+def scraping():
 	#Configuration du scraper, ne pas modifier
-	reddit=praw.Reddit(client_id="v7xiCUUDI3vEmg",client_secret="5Q6FHHJT-SW0YRnEmtWkekWsxHU",
-	password="Blorp86",user_agent="PhotoScraper",username="scrapelord")
+	reddit=praw.Reddit(client_id='v7xiCUUDI3vEmg',client_secret='5Q6FHHJT-SW0YRnEmtWkekWsxHU',
+	password='Blorp86',user_agent='PhotoScraper',username='scrapelord')
 
 	#Configuration recherche reddit
-	target_sub=reddit.subreddit("EarthPorn") #Subreddit cible
-	#récuperer les paramètres envoyés par la JavaScript
-	country=request.args.get('country')           #Pays cible
+	target_sub=reddit.subreddit('EarthPorn') #Subreddit cible
+	#Paramètres de la requête Javascript
+	country=request.args.get('country')
 	country_code=request.args.get('country_code')
-	query="title:"+country
-	print("\033[92m"+target_sub.display_name+"\033[0m","\nRésultats de recherche pour les soumissions reddit avec: ",query,"\n")
+	query='title:'+country
+	print('\033[92m'+target_sub.display_name+'\033[0m','\nRésultats de recherche pour les soumissions reddit avec: ',query,'\n')
 
 	#Configuration TreeTagger
-	reddit_tagger=treetaggerwrapper.TreeTagger(TAGLANG="en",TAGDIR=os.getcwd()+"/TreeTagger") #Se placer dans un dossier pour que le chemin TAGDIR fonctionne
+	reddit_tagger=treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR=os.getcwd()+'/TreeTagger')
+	#Se placer dans un dossier pour que le chemin TAGDIR fonctionne
 
 	#Résultats de la recherche dans le subreddit
 	test_posts=target_sub.search(query,limit=25)
 	dic_results={}	#Dico stockant tous les résultats de recherche (results) et les informations nécessaires (head)
-	dic_results['head']={}    #head con
+	dic_results['head']={}
 	dic_results['head']['total']=0
-	dic_results['head']['country']={}                     #pour la génération de la carte (lat,lng)
+	dic_results['head']['country']={}	#Pour la génération de la carte (lat,lng)
 	dic_results['head']['country']['name']=country
 
-
-	search_res=requests.get("http://api.geonames.org/searchJSON?q="+country+"&username=scrapelord",auth=("scrapelord","Blorp86"))
+	#Coordonnées pays
+	search_res=requests.get('http://api.geonames.org/searchJSON?q='+country+'&username=scrapelord',auth=('scrapelord','Blorp86'))
 	search_res=search_res.json()
 	dic_results['head']['country']['lng']=search_res['geonames'][0]['lng']
 	dic_results['head']['country']['lat']=search_res['geonames'][0]['lat'] 
 	dic_results['results']=[]
-	for post in test_posts: #Objets 'submission'
-		if re.search(".*"+country+"[.,/[( ]",post.title): #Pays suivi de '.' ',' '/' '[' '(' ou ' '
-			res=re.search("^([^[(]+)",post.title) #Match tous les caractères depuis le début de la ligne, excluant [ et (
+
+	#Objets 'submission' renvoyés par la recherche
+	for post in test_posts:
+		if re.search('.*'+country+'[.,/[( ]',post.title): #Pays suivi de '.' ',' '/' '[' '(' ou ' '
+			#Match tous les caractères depuis le début de la ligne sauf [OC] et s'arrête au premier [ ou (
+			res=re.search('^(?:\[OC\])?([^[(]+)',post.title)
 			if (res):
 				print(res.group(1))
 
@@ -97,27 +126,26 @@ def scraping():          #la fonction qui sera appeler par le JavaScript
 				pprint.pprint(reddit_tags)
 
 				#Recherche des lieux potentiels
-				if (country.lower() in (t[2].lower() for t in reddit_tags)):  #pour éviter que le programme plante s'il trouve pas le nom de pays
+				if (country.lower() in (t[2].lower() for t in reddit_tags)):  #Garantir la présence du mot 'NomDuPays'
 					title_split=[t[2].lower() for t in reddit_tags].index(country.lower()) #Position du mot 'NomDuPays'
 					location_list=[]
 					#Recherche prioritaire du lieu: la liste jusqu'au mot 'NomDuPays'
-					print(IterTagList(location_list,more_itertools.peekable(reddit_tags[:title_split])))
 					IterTagList(location_list,more_itertools.peekable(reddit_tags[:title_split])) #Permet de regarder l'élément suivant de la liste
-					location_list.reverse() #Les noms propres les plus proches de 'NomDuPays' sont prioritaires
+					#location_list.reverse() #Les noms propres les plus proches de 'NomDuPays' sont prioritaires
 					#Priorité secondaire: la liste à la suite du mot 'NomDuPays'
 					if len(reddit_tags) > title_split+1:
 						IterTagList(location_list,more_itertools.peekable(reddit_tags[title_split+1:]))
 					#Priorité tertiaire: commentaire(s) du redditor qui a posté la photo
 					for comment in post.comments.list(): #Liste du parcours en largeur de l'arborescence de commentaires
-						location=""
-						if isinstance(comment,praw.models.MoreComments): #On ignore les objets MoreComments (pour le moment?)
+						location=''
+						if isinstance(comment,praw.models.MoreComments): #On ignore les objets MoreComments
 							continue
 						if comment.is_submitter:
 							comment_tags=treetaggerwrapper.make_tags(reddit_tagger.tag_text(comment.body),exclude_nottags=True)
 							IterTagList(location_list,more_itertools.peekable(comment_tags))
-					print("Lieux trouvés:",end="")
+					print('Lieux trouvés:',end='')
 					pprint.pprint(location_list)
-					print("")
+					print('')
 
 					#GeoNames
 					if location_list:
@@ -125,25 +153,17 @@ def scraping():          #la fonction qui sera appeler par le JavaScript
 							dic_tmp={} #Initialisé en dehors de la fonction GeoNamesQuery pour pouvoir comparer après l'appel
 							if GeoNamesQuery(loc,country_code,dic_results,dic_tmp,post.url):
 								break
-						"""Pas de résultat après avoir parcouru toute la liste: on passe à une fuzzy search pour prendre en compte
-						les potentielles erreurs d'orthographe"""
+						'''Pas de résultat après avoir parcouru toute la liste: on passe à une fuzzy search pour prendre en compte
+						les potentielles erreurs d'orthographe'''
 						if not dic_tmp:
 							for loc in location_list:
 								dic_tmp={}
 								if GeoNamesQuery(loc,country_code,dic_results,dic_tmp,post.url,fuzzy=True):
 									break
-					#A FAIRE
-					#Utiliser FeatureClass pour distinguer les types de résultat GeoNames, et hiérarchiser
 					else:
-						print("")
+						print('')
 
-	#Stockage du JSON dans un fichier
-	"""if os.path.exists("GN_Results.json"):	#Pour mettre à jour le fichier à chaque exécution (temporaire)
-		os.remove("GN_Results.json")
-	with open("GN_Results.json", "a") as file:
-		json.dump(dic_results,file)
-	"""
-	return jsonify(dic_results)                  #renvoyer un objet json
+	return jsonify(dic_results)	#Renvoyer directement un objet JSON
 
-if __name__ == "__main__" :
+if __name__ == '__main__' :
 	app.run(debug=True,port=5000)
