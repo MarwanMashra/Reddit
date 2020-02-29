90de4562c9910fdfa02058be91902c4ff294560b
torpid <troyfau@gmail.com>
2020-02-29 23:57:41 +0100

    Boucle de test qui rajoute, affiche puis enlève un champs à deux documents

--- Reddit_projet/update.py
+++ Reddit_projet/update.py
@@ -9,23 +9,31 @@ update_dic = {}
 #Les deux clés doivent être présentes
 update_dic['search_version'] = '1.00'
 update_dic['updates'] = [] #Si liste vide, pas d'erreurs
-"""update_dic['updates'] = [{
+update_dic['updates'] = [{
 							'field': 'test_result',
 							'newvalue': 'OK',
 							'url': ['https://i.redd.it/7h95m06db2w31.png']
-							}]
-"""
-"""update_dic['updates'] = [{
+							},
+							{
 							'field': 'test_result',
 							'newvalue': 'NOT_OK',
 							'url': ['https://i.redd.it/6wbjlunbdlc41.jpg']
 						}]
-"""
-updater = mongo.MongoUpd(update_dic)
-updater.updatedb('Resultats_RGN')
 
+updater = mongo.MongoUpd(update_dic)
+updater.updatedb('Resultats_RGN') #Rajout du champs 'test_result'
+
+#Extraction de documents
 document = mongo.MongoLoad({'search_version': '1.00', 'test_result': 'OK'},
 						   {'search_version': 1, 'country': 1, 'title': 1, 'location_list': 1,
 							'name': 1, 'location': 1, '_id': 0})
 test_case = document.retrieve('Resultats_RGN',limit=5)
 pprint.pprint(test_case)
+
+update_dic['updates'] = [{
+							'field': 'test_result',
+							'newvalue': '',
+							'url': ['https://i.redd.it/7h95m06db2w31.png','https://i.redd.it/6wbjlunbdlc41.jpg']
+						}]
+updater.reinit(update_dic)
+updater.unsetdb('Resultats_RGN') #Suppression du champs 'test_result'
