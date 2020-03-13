b53870653b53a8a8cb845a187f3e890cd7f5afb4
torpid <troyfau@gmail.com>
2020-03-13 07:59:29 +0100

    Rajouté recharge de l'attribut 'testers' des documents NOT_OK pour permettre de tester la page test-expert

--- Reddit_projet/update.py
+++ Reddit_projet/update.py
@@ -4,13 +4,12 @@
 import mongo, pprint
 
 
-#SCRIPT TEST
+#Script de recharge du champs 'testers' des documents NOT_OK pour les tests
 
 """Classe de mise à jour simplifiée et plus générique.
 On fait une mise à jour seulement sur un champs, avec une nouvelle valeur.
 """
 update_dic = {}
-update_dic['search_version'] = '1.00'
 update_dic['update'] = 'test_result'
 update_dic['newvalue'] = 'OK'
 update_dic['id_field'] = {
@@ -22,31 +21,39 @@ update_dic['other_field'] = {
 								'value': '1.00'
 							}
 updater = mongo.MongoUpd(update_dic)
-updater.updatedb('Resultats_RGN') #Rajout du champs 'test_result'
+updater.updatedb('Resultats_RGN','$set') #Rajout du champs 'test_result'
 print('Succès.')
-# update_dic['updates'] = [{
-# 							'field': 'test_result',
-# 							'newvalue': 'OK',
-# 							'url': ['https://i.redd.it/7h95m06db2w31.png']
-# 							},
-# 							{
-# 							'field': 'test_result',
-# 							'newvalue': 'NOT_OK',
-# 							'url': ['https://i.redd.it/6wbjlunbdlc41.jpg',
-# 									'https://i.redd.it/2qhzpuf77se41.jpg']
-# 							},
-# 							{
-# 							'field': 'testers',
-# 							'newvalue': 2**0 + 2**1 + 2**2,
-# 							'url': ['https://i.redd.it/6wbjlunbdlc41.jpg',
-# 									'https://i.redd.it/2qhzpuf77se41.jpg']
-# 							}]
-
-#Extraction de documents
-# document = mongo.MongoLoad({'search_version': '1.00', 'test_result': 'NOT_OK', 'testers': {'$gte': 4}},
-# 						   {'search_version': 1, 'country': 1, 'title': 1,
-# 							'name': 1, 'location': 1, 'testers': 1, '_id': 0})
-# case = document.retrieve('Resultats_RGN',limit=5)
-# test_case = [c for c in case if c['testers'] & (1<<2)]
-# pprint.pprint(test_case)
-# print("")
+updater.reinit({
+					'update': 'test_result',
+					'newvalue': 'NOT_OK',
+					'id_field': {
+									'name': 'img_url',
+									'values': ['https://i.imgur.com/3LDmMr7.jpg',
+											   'https://i.redd.it/lnckwb6v4ee41.jpg',
+											   'https://i.redd.it/6wbjlunbdlc41.jpg',
+											   'https://i.redd.it/2qhzpuf77se41.jpg']
+								},
+					'other_field': {
+									'name': 'search_version',
+									'value': '1.00'
+									}
+				})
+updater.updatedb('Resultats_RGN','$set')
+print('Succès.')
+updater.reinit({
+					'update': 'testers',
+					'newvalue': 2**0 + 2**1 + 2**2,
+					'id_field': {
+									'name': 'img_url',
+									'values': ['https://i.imgur.com/3LDmMr7.jpg',
+											   'https://i.redd.it/lnckwb6v4ee41.jpg',
+											   'https://i.redd.it/6wbjlunbdlc41.jpg',
+											   'https://i.redd.it/2qhzpuf77se41.jpg']
+								},
+					'other_field': {
+									'name': 'search_version',
+									'value': '1.00'
+									}
+				})
+updater.updatedb('Resultats_RGN','$set')
+print('Succès.')
